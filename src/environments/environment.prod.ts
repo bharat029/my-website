f79f1092fe0cbac311c373b7e3609917105ee550
Bharathan Mudaliar <vbharathan.2013@gmail.com>
2022-01-22 10:15:51 +0530

    angular material and angular fire setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'my-website-dev-2aa35',
+    appId: '1:390739824653:web:0a43c0d5da4823dd4f0544',
+    storageBucket: 'my-website-dev-2aa35.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyCOsqCtUXbxPCjaTkw1Z1CXq90pIrL3GxI',
+    authDomain: 'my-website-dev-2aa35.firebaseapp.com',
+    messagingSenderId: '390739824653',
+  },
   production: true
 };
