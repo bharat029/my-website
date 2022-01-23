d8fc63d63315bce657c6adb55a7716d2657f973e
Bharathan Mudaliar <vbharathan.2013@gmail.com>
2022-01-23 14:00:40 +0530

    theme setup

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
 
 
 @NgModule({
-  declarations: [
-    LandingComponent
-  ],
-  imports: [
-    CommonModule,
-    LandingRoutingModule
-  ]
+  declarations: [LandingComponent],
+  imports: [CommonModule, FlexLayoutModule, LandingRoutingModule],
 })
-export class LandingModule { }
+export class LandingModule {}
